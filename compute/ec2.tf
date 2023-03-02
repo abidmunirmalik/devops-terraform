b1958cb6043707c7fd4e68bc1dd84b433a0078fb
Abid Malik <abidmunirmalik@gmail.com>
2023-03-02 12:39:46 -0600

    modified ec2.tf

--- compute/ec2.tf
+++ compute/ec2.tf
@@ -5,38 +5,8 @@ resource "aws_instance" "webserver" {
   vpc_security_group_ids      = [aws_security_group.sg.id]
   subnet_id                   = data.aws_subnet.public_subnet_1a.id
   key_name                    = aws_key_pair.keypair.id
-
-  provisioner "local-exec" {
-    command    = "echo ${self.private_ip} >> ec2s.txt"
-    on_failure = continue
-  }
-
-  provisioner "file" {
-    source      = "httpd.sh"
-    destination = "/tmp/httpd.sh"
-    on_failure  = continue
-  }
-  connection {
-    type        = "ssh"
-    host        = aws_instance.webserver.public_ip
-    user        = "ec2-user"
-    private_key = file("/Users/malik/.ssh/aws-golden-key")
-    timeout     = "5m"
-  }
-
-  provisioner "remote-exec" {
-    inline     = ["chmod u+x /tmp/httpd.sh", "/tmp/httpd.sh"]
-    on_failure = continue
-  }
-
+  
   tags = {
     "Name" = "${var.prefix}-${var.ec2_name}"
   }
 }
-
-
-# null_resource Resource
-  resource "null_resource" "on_ec2_id_change" {
-  triggers = {
-    id = aws_instance.webserver.id
-  }
